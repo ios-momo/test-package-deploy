fd60f86900b153836811e75716c9409cffde53b6
ruda8711 <127097067+ruda8711@users.noreply.github.com>
2023-06-08 17:24:39 +0900

    init

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "test-package-deploy",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "test-package-deploy",
+            targets: [
+                "app-framework",
+                "flutter-framework",
+                "flutter-plugin-framework",
+                "flutter-toast-framework",
+                "toast-framework",
+                "url-launcher-framework",
+            ]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "app-framework", path: "./App.xcframework"),
+        .binaryTarget(name: "flutter-framework", path: "./Flutter.xcframework"),
+        .binaryTarget(name: "flutter-plugin-framework", path: "./FlutterPluginRegistrant.xcframework"),
+        .binaryTarget(name: "flutter-toast-framework", path: "./fluttertoast.xcframework"),
+        .binaryTarget(name: "toast-framework", path: "./Toast.xcframework"),
+        .binaryTarget(name: "url-launcher-framework", path: "./url_launcher_ios.xcframework"),
+    ]
+)
